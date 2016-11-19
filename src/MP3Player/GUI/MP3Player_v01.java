4ac68f553163b76255cd144ab3ad65f1e55bdb72
Chalach <alex.programmieren@gmail.com>
2016-11-19 01:04:34 +0100

    PlayList

--- src/MP3Player/GUI/MP3Player_v01.java
+++ src/MP3Player/GUI/MP3Player_v01.java
@@ -1,16 +1,53 @@
 package MP3Player.GUI;
 
 import jaco.mp3.player.MP3Player;
+
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.io.File;
 import java.util.ArrayList;
 
-public class MP3Player_v01 {
+public class MP3Player_v01 extends Component {
+    private static File path = getPath();
+
+    private static File getPath(){
+        JFileChooser fileChooser = new JFileChooser();
+
+        // Setzt den Pfad auf den zu letzt geöffneten
+        fileChooser.setCurrentDirectory(new java.io.File("."));
+
+        // Erlaubt es Dateien und Ordner zu lesen
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+
+        // Reiter "Alle-Daten" wird dem Nutzer zugänglich gemacht
+        fileChooser.setAcceptAllFileFilterUsed(true);
+
+        // Filter, welche Dateien angezeigt werden soll
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("MP3-Datein", "mp3");
+        FileNameExtensionFilter filter2 = new FileNameExtensionFilter("WAV-Datein", "wav");
+
+        // Filter hizufügen
+        fileChooser.addChoosableFileFilter(filter);
+        fileChooser.addChoosableFileFilter(filter2);
+
+        // Öffnet das Dialog Fenster
+        fileChooser.showOpenDialog(null);
+        /*
+        if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            System.out.println("getCurrentDirectory(): " +  fileChooser.getCurrentDirectory());
+            System.out.println("getSelectedFile() : " +  fileChooser.getSelectedFile());
+        }
+        else {
+            System.out.println("No Selection ");
+        }*/
+
+        return fileChooser.getSelectedFile().getAbsoluteFile();
+    }
 
     private static ArrayList<String> musicFiles(){
         ArrayList<String> songList = new ArrayList<>();
-        File test = new File("Music\\");
+        File test = new File(path + "\\");
         String[] DIR = test.list();
 
         for (int i = 0; i < DIR.length; i++){
@@ -41,20 +78,27 @@ public class MP3Player_v01 {
             player.addToPlayList(new File("Music\\" + myPlayList.get(i)));
         }
 
-        // GUI
-        player.setBorder(BorderFactory.createEmptyBorder(15, 100, 15, 100));
+        // GUI vom Player
+        player.setBorder(BorderFactory.createEmptyBorder(15, 50, 15, 50));
 
 
+        // Fenster
         JFrame frame = new JFrame("MP3 Player");
+
+        // Frame Icon
         frame.setIconImage(new ImageIcon("C:\\Users\\Chalach\\Desktop\\MP3Player\\ImageIcon\\icon.jpg").getImage());
-        //JButton button = new JButton("Show song");
-        //button.addActionListener(new TestActionListener());
-        //frame.add(button);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(player);
-        frame.setLayout(new FlowLayout());
+
+        // Button
+        JButton button = new JButton("Show song");
+        button.addActionListener(new TestActionListener());
+        frame.add(button);
+
+        // Eigentliches Fenster
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // -> legt fest, was passiert, wenn man den Dialog/Programm schließt
+        frame.getContentPane().add(player); // -> fügt den Player hinzu
+        frame.setLayout(new FlowLayout()); // -> notwendig für einen Button
         frame.pack();
-        frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null); // -> legt fest, wo das Fenster auftauchen soll: hier ist es in der Mitte
         frame.setVisible(true);
     }
 }
--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,18 +1,37 @@
 package Test;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Test {
+public class Test extends Component {
     public static void main(String[] args) throws IOException {
-        readFile();
+        String c = readFile2();
+        System.out.println(c);
+        DemoJFileChooser panel = new DemoJFileChooser();
     }
-    private static void readFile() throws IOException {
+    private void readFile() throws IOException {
         JFileChooser chooser = new JFileChooser();
-        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY); //chooser soll nur Dateien öffnen
-        int retrival = chooser.showOpenDialog(null); //Auswahl Dialog öffnen
+        //chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); //chooser soll nur Dateien öffnen
+        //int retrival = chooser.showOpenDialog(null); //Auswahl Dialog öffnen
+
+        // Filter, welche Dateien angezeigt werden soll
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("MP3-Datein", "mp3");
+        FileNameExtensionFilter filter2 = new FileNameExtensionFilter("WAV-Datein", "wav");
+
+        // Filter hizufügen
+        chooser.addChoosableFileFilter(filter);
+        chooser.addChoosableFileFilter(filter2);
+        if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            System.out.println("getCurrentDirectory(): " +  chooser.getCurrentDirectory());
+            System.out.println("getSelectedFile() : " +  chooser.getSelectedFile());
+        }
+        else {
+            System.out.println("No Selection ");
+        }/*
         if (retrival == JFileChooser.APPROVE_OPTION) {
             try (FileReader fr = new FileReader(chooser.getSelectedFile())) { //Reader mit Pfad der Datei öffnen
                 BufferedReader br = new BufferedReader(fr);
@@ -26,6 +45,23 @@ public class Test {
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
+        }*/
+    }
+
+    private static String readFile2() throws IOException {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY); //chooser soll nur Dateien öffnen
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("MP3-Datein", "mp3");
+        FileNameExtensionFilter filter2 = new FileNameExtensionFilter("WAV-Datein", "wav");
+
+        // Filter hizufügen
+        chooser.addChoosableFileFilter(filter);
+        chooser.addChoosableFileFilter(filter2);
+        int retrival = chooser.showOpenDialog(null); //Auswahl Dialog öffnen
+        if (retrival == JFileChooser.APPROVE_OPTION) {
+            String path = chooser.getSelectedFile().getAbsolutePath();
+            return path;
         }
+        return null;
     }
 }
